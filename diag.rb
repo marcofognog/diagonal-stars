4fa5d22930e2cbdf48674a59d1e4952fe8677fa3
Marcao <marcofognog@gmail.com>
2018-06-18 13:35:21 -0300

    Ah, of course, from left to right it is an identity function

--- diag.rb
+++ diag.rb
@@ -30,11 +30,7 @@ require 'test/unit'
 def doline(n, pos)
    line = []
    n.times do |index|
-     if pos == 0 && index == 0 ||
-        pos == 1 && index == 1 ||
-        pos == 2 && index == 2 ||
-        pos == 3 && index == 3 ||
-        pos == 4 && index == 4 ||
+     if pos == index ||
         pos == 0 && index == 4 ||
         pos == 1 && index == 3 ||
         pos == 3 && index == 1 ||
