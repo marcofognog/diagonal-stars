713f5d8e382dccfd65f3f53da8c00447eae47f6e
Marcao <marcofognog@gmail.com>
2018-06-18 13:46:07 -0300

    alright, fixed the hard coded part, all passing now

--- diag.rb
+++ diag.rb
@@ -30,7 +30,7 @@ require 'test/unit'
 def doline(n, pos)
    line = []
    n.times do |index|
-     if pos == index || (index + pos) == 4
+     if pos == index || (index + pos) == (n - 1)
        char = '*'
      else
        char = '-'
