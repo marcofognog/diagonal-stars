a44fbe5c0dc75af0fcc7e91fad28d4d1c4c12c95
Marcao <marcofognog@gmail.com>
2018-06-18 13:00:14 -0300

    alright, this is a start

--- diag.rb
+++ diag.rb
@@ -24,3 +24,28 @@
 # n = 3
 # *-*
 # -*-
+
+require 'test/unit'
+
+def diag(n)
+  str = []
+  n.times do
+    str << '-----'
+  end
+  str.join("\n") + "\n"
+end
+
+class DiagonalStar < Test::Unit::TestCase
+
+  def test_first
+    expected =<<END
+-----
+-----
+-----
+-----
+-----
+END
+    assert_equal(expected, diag(5))
+  end
+
+end
